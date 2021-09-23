e843fabf079c58173bcc5ff65cf3d819b8b8b14f
Laurent Abbal <laurent@abbal.com>
2021-09-23 14:53:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-setup(
-    name="chronocode",
-    version="0.0.1",
-    description="Module Chronocode",
-    long_description=parent_dir.joinpath("README.md").read_text(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/laurentabbal/module-chrono",
-    author="xxx",
-    author_email="xxx@gmail.com",
-    license="MIT License",
-    packages=find_packages(exclude=("assets", "notebooks", "prints", "script")),
-    install_requires=parent_dir.joinpath("requirements.txt").read_text().splitlines(),
-    classifiers=[
-        "Intended Audience :: Science/Research",
-    ],
-)
-
 from setuptools import setup
 
 setup(
