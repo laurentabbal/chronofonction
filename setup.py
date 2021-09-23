f9e9b40c9b441f9f1b28b8d3fe3033db259d2487
Laurent Abbal <laurent@abbal.com>
2021-09-23 14:53:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from setuptools import setup, find_packages
-from pathlib import Path
-
-parent_dir = Path(__file__).resolve().parent
-
 setup(
     name="chronocode",
     version="0.0.1",
@@ -18,4 +13,25 @@ setup(
     classifiers=[
         "Intended Audience :: Science/Research",
     ],
+)
+
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='chronofonction',
+    url='https://github.com/laurentabbal/chronofonction',
+    author='Laurent Abbal',
+    author_email='laurent@abbal.com',
+    # Needed to actually package something
+    packages=['chronofonction'],
+    # Needed for dependencies
+    install_requires=['time'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Mesure du temps d\'execution d\'une fonction',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
 )
